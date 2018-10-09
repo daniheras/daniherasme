44e7deaadb43ccb96e48632c9adacb702970b2e9
Dani Heras <danielherascabrera@gmail.com>
2018-10-09 22:30:42 +0200

    feat: creating basic structure of the app
    Wrapping the app with create-react-app and ts.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+  public render() {
+    return (
+      <div className="App">
+        Dani Heras
+      </div>
+    );
+  }
+}
+
+export default App;
